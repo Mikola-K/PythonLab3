5cae1d79d7c7ed2ca3554157ed041c841722f0ad
Mikola-K <hotoneshot.ua@gmail.com>
2021-04-04 21:38:26 +0300

    adding code for lab4

---
+++ modules/bank_type.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class BankType(Enum):
+    NATIONAL = 0
+    COMERCIAL = 1
---
+++ modules/credit_term.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class CretidTerm(Enum):
+    LONG_TERM = 0
+    MEDIUM_TERM = 1
+    SHORT_TERM = 2
